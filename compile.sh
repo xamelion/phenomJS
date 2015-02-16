7cb0bde0c37a0bf1e23c6f812ad6d50d9151ae0a
Виталий Канев <link_86@mail.ru>
2015-02-16 07:29:53 +0300

    Structure Project

--- compile.sh
+++ compile.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
 # Компиляция под LINUX64
+
+# Переносим из src в build и pbgetv
 zip -r -j build/phenomjs.nw src
-cat nodewebkit/LINUX64/nw build/phenomjs.nw  > build/phenomjs_linux_64 && chmod +x build/phenomjs_linux_64
-build/phenomjs_linux_64
+
+# Компилируем под 64 битный линукс и назанчаем права на доступ
+cat ~/NodeWebkit/nw build/phenomjs.nw  > bin/LINUX64/phenomjs && chmod +x bin/LINUX64/phenomjs
+
+# Копируем дополнительные файлы
+cp ~/NodeWebkit/libffmpegsumo.so    bin/LINUX64/libffmpegsumo.so
+cp ~/NodeWebkit/icudtl.dat          bin/LINUX64/icudtl.dat
+cp ~/NodeWebkit/nw.pak              bin/LINUX64/nw.pak
+cp -R ~/NodeWebkit/locales          bin/LINUX64/locales
+
+# Запускаем приложение
+bin/LINUX64/phenomjs
